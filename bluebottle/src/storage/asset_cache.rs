c41b5fd1a9dba04f704734b1038378a8098e832a
chillfish8 <hburt2003@gmail.com>
2025-12-23 18:25:59 +0000

    Make asset cache global functions

--- bluebottle/src/storage/asset_cache.rs
+++ bluebottle/src/storage/asset_cache.rs
@@ -1,4 +1,4 @@
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::str::FromStr;
 use std::time::UNIX_EPOCH;
 
@@ -7,121 +7,107 @@ use snafu::ResultExt;
 
 type AssetId = ArrayString<64>;
 
-#[derive(Debug, Copy, Clone)]
-/// Cache assets and images locally on disk rather than fetching from the server.
-pub struct AssetProxyCache {
-    cache_directory: &'static Path,
+/// Try retrieve a cached entry with the given path.
+pub fn try_get(path: &str) -> Option<Vec<u8>> {
+    let asset_id: AssetId = blake3::hash(path.as_bytes()).to_hex();
+    let cache_path = super::directory::paths().asset_cache_dir();
+    let path = cache_path.join(asset_id);
+    std::fs::read(&path).ok()
 }
 
-impl AssetProxyCache {
-    /// Open the [AssetProxyCache] in using the global state paths.
-    pub fn open() -> Result<Self, snafu::Whatever> {
-        let paths = super::directory::paths();
-        Ok(Self {
-            cache_directory: paths.data_dir(),
-        })
+/// Try insert a new asset into the cache directory.
+pub fn insert(path: &str, data: &[u8]) {
+    let asset_id: AssetId = blake3::hash(path.as_bytes()).to_hex();
+    let cache_path = super::directory::paths().asset_cache_dir();
+    let path = cache_path.join(asset_id);
+    if let Err(e) = std::fs::write(path, data) {
+        tracing::warn!(error = %e, "failed to write asset to cache");
+    }
+}
+
+/// Prune the cache to be up to the given capacity.
+///
+/// Returns the number of entries removed.
+pub fn prune_to(target_size: u64) -> Result<usize, snafu::Whatever> {
+    let mut all_entries = list_all_entries()?;
+    all_entries.sort_unstable_by_key(|entry| access_time(&entry.metadata));
+
+    let total_size: u64 = all_entries.iter().map(|entry| entry.metadata.len()).sum();
+    let delta_size = total_size.saturating_sub(target_size);
+    if delta_size == 0 {
+        return Ok(0);
     }
 
-    /// Try retrieve a cached entry with the given path.
-    pub fn try_get(&self, path: &str) -> Option<Vec<u8>> {
-        let asset_id: AssetId = blake3::hash(path.as_bytes()).to_hex();
-        let path = self.cache_directory.join(asset_id);
-        std::fs::read(&path).ok()
-    }
+    let mut removed = 0;
+    let mut bytes_freed = 0;
+    for entry in all_entries {
+        if bytes_freed > delta_size {
+            break;
+        }
 
-    /// Try insert a new asset into the cache directory.
-    pub fn insert(&self, path: &str, data: &[u8]) {
-        let asset_id: AssetId = blake3::hash(path.as_bytes()).to_hex();
-        let path = self.cache_directory.join(asset_id);
-        if let Err(e) = std::fs::write(path, data) {
-            tracing::warn!(error = %e, "failed to write asset to cache");
+        tracing::debug!(asset_id = %entry.id, "removing cached asset");
+
+        match std::fs::remove_file(&entry.path) {
+            Ok(_) => {
+                removed += 1;
+                bytes_freed += entry.metadata.len();
+            },
+            Err(err) => {
+                tracing::warn!(error = %entry.id, err = %err, "failed to remove cached asset");
+            },
         }
     }
 
-    /// Prune the cache to be up to the given capacity.
-    ///
-    /// Returns the number of entries removed.
-    pub fn prune_to(&self, target_size: u64) -> Result<usize, snafu::Whatever> {
-        let mut all_entries = self.list_all_entries()?;
-        all_entries.sort_unstable_by_key(|entry| access_time(&entry.metadata));
+    Ok(removed)
+}
 
-        let total_size: u64 = all_entries.iter().map(|entry| entry.metadata.len()).sum();
-        let delta_size = total_size.saturating_sub(target_size);
-        if delta_size == 0 {
-            return Ok(0);
-        }
+/// Returns the total size of the cached entries.
+pub fn usage() -> Result<u64, snafu::Whatever> {
+    let total = list_all_entries()?
+        .into_iter()
+        .map(|entry| entry.metadata.len())
+        .sum();
+    Ok(total)
+}
 
-        let mut removed = 0;
-        let mut bytes_freed = 0;
-        for entry in all_entries {
-            if bytes_freed > delta_size {
-                break;
-            }
+/// List all entries within the cache along with their metadata.
+fn list_all_entries() -> Result<Vec<AssetEntry>, snafu::Whatever> {
+    let cache_directory = super::directory::paths().asset_cache_dir();
+    let entries = std::fs::read_dir(cache_directory)
+        .whatever_context("failed to read cache directory")?;
 
-            tracing::debug!(asset_id = %entry.id, "removing cached asset");
-
-            match std::fs::remove_file(&entry.path) {
-                Ok(_) => {
-                    removed += 1;
-                    bytes_freed += entry.metadata.len();
-                },
-                Err(err) => {
-                    tracing::warn!(error = %entry.id, err = %err, "failed to remove cached asset");
-                },
-            }
-        }
-
-        Ok(removed)
-    }
-
-    /// Returns the total size of the cached entries.
-    pub fn usage(&self) -> Result<u64, snafu::Whatever> {
-        let total = self
-            .list_all_entries()?
-            .into_iter()
-            .map(|entry| entry.metadata.len())
-            .sum();
-        Ok(total)
-    }
-
-    /// List all entries within the cache along with their metadata.
-    fn list_all_entries(&self) -> Result<Vec<AssetEntry>, snafu::Whatever> {
-        let entries = std::fs::read_dir(self.cache_directory)
-            .whatever_context("failed to read cache directory")?;
-
-        let mut all_entries = Vec::new();
-        for file in entries {
-            let entry = match file {
-                Ok(entry) => entry,
-                Err(err) => {
-                    tracing::warn!(error = %err, "cannot get entry info");
-                    continue;
-                },
-            };
-
-            let path = entry.path();
-            let file_name = entry.file_name();
-            let id = match AssetId::from_str(&file_name.to_string_lossy()) {
-                Ok(asset_id) => asset_id,
-                Err(_) => {
-                    tracing::warn!(file_name = ?file_name, "invalid cache file name found");
-                    continue;
-                },
-            };
-
-            let metadata = entry
-                .metadata()
-                .whatever_context("failed to get cache entry metadata")?;
-
-            if metadata.is_dir() {
+    let mut all_entries = Vec::new();
+    for file in entries {
+        let entry = match file {
+            Ok(entry) => entry,
+            Err(err) => {
+                tracing::warn!(error = %err, "cannot get entry info");
                 continue;
-            }
+            },
+        };
 
-            all_entries.push(AssetEntry { id, metadata, path });
+        let path = entry.path();
+        let file_name = entry.file_name();
+        let id = match AssetId::from_str(&file_name.to_string_lossy()) {
+            Ok(asset_id) => asset_id,
+            Err(_) => {
+                tracing::warn!(file_name = ?file_name, "invalid cache file name found");
+                continue;
+            },
+        };
+
+        let metadata = entry
+            .metadata()
+            .whatever_context("failed to get cache entry metadata")?;
+
+        if metadata.is_dir() {
+            continue;
         }
 
-        Ok(all_entries)
+        all_entries.push(AssetEntry { id, metadata, path });
     }
+
+    Ok(all_entries)
 }
 
 struct AssetEntry {
@@ -147,12 +133,11 @@ mod tests {
         crate::storage::directory::init_paths(Some(tmp_dir.path().to_path_buf()))
             .unwrap();
 
-        let cache = AssetProxyCache::open().unwrap();
-        cache.insert("test1.txt", b"hello world 1");
-        cache.insert("test2.txt", b"hello world 2");
+        insert("test1.txt", b"hello world 1");
+        insert("test2.txt", b"hello world 2");
 
-        let content1 = cache.try_get("test1.txt").unwrap();
-        let content2 = cache.try_get("test2.txt").unwrap();
+        let content1 = try_get("test1.txt").unwrap();
+        let content2 = try_get("test2.txt").unwrap();
         assert_eq!(content1, b"hello world 1");
         assert_eq!(content2, b"hello world 2");
     }
@@ -163,11 +148,10 @@ mod tests {
         crate::storage::directory::init_paths(Some(tmp_dir.path().to_path_buf()))
             .unwrap();
 
-        let cache = AssetProxyCache::open().unwrap();
-        cache.insert("test1.txt", b"hello world 1");
-        cache.insert("test2.txt", b"hello world 2");
+        insert("test1.txt", b"hello world 1");
+        insert("test2.txt", b"hello world 2");
 
-        let total_size = cache.usage().unwrap();
+        let total_size = usage().unwrap();
         assert_eq!(total_size, 26);
     }
 
@@ -177,18 +161,17 @@ mod tests {
         crate::storage::directory::init_paths(Some(tmp_dir.path().to_path_buf()))
             .unwrap();
 
-        let cache = AssetProxyCache::open().unwrap();
-        cache.insert("test1.txt", b"hello world 1");
+        insert("test1.txt", b"hello world 1");
         std::thread::sleep(std::time::Duration::from_millis(200));
-        cache.insert("test2.txt", b"hello world 2");
+        insert("test2.txt", b"hello world 2");
 
-        let total_size = cache.usage().unwrap();
+        let total_size = usage().unwrap();
         assert_eq!(total_size, 26);
 
-        let n_removed = cache.prune_to(14).unwrap();
+        let n_removed = prune_to(14).unwrap();
         assert_eq!(n_removed, 1);
 
-        assert!(cache.try_get("test1.txt").is_none());
-        assert!(cache.try_get("test2.txt").is_some());
+        assert!(try_get("test1.txt").is_none());
+        assert!(try_get("test2.txt").is_some());
     }
 }
--- bluebottle/src/storage/directory.rs
+++ bluebottle/src/storage/directory.rs
@@ -19,7 +19,7 @@ pub fn init_paths(base_path: Option<PathBuf>) -> Result<(), snafu::Whatever> {
 
     tracing::info!(
         config = %paths.config_dir().display(),
-        cache = %paths.cache_dir().display(),
+        cache = %paths.asset_cache_dir().display(),
         data = %paths.data_dir().display(),
         "directory paths have been initialised"
     );
@@ -35,15 +35,16 @@ pub fn paths() -> &'static DirectoryPaths {
 /// Manages file paths for app data storage.
 pub struct DirectoryPaths {
     config_dir: PathBuf,
-    cache_dir: PathBuf,
+    asset_cache_dir: PathBuf,
     data_dir: PathBuf,
 }
 
 impl DirectoryPaths {
     fn from_explicit_path(path: PathBuf) -> Self {
+        let cache_dir = path.join("cache");
         Self {
             config_dir: path.join("config"),
-            cache_dir: path.join("cache"),
+            asset_cache_dir: cache_dir.join("assets"),
             data_dir: path.join("data"),
         }
     }
@@ -51,9 +52,11 @@ impl DirectoryPaths {
     fn from_sniffed() -> Self {
         let paths = directories::ProjectDirs::from("com", "chillfish8", "Bluebottle")
             .expect("Could not determine project directories");
+
+        let cache_dir = paths.cache_dir();
         Self {
             config_dir: paths.config_dir().to_path_buf(),
-            cache_dir: paths.cache_dir().to_path_buf(),
+            asset_cache_dir: cache_dir.join("assets"),
             data_dir: paths.data_dir().to_path_buf(),
         }
     }
@@ -62,8 +65,8 @@ impl DirectoryPaths {
         std::fs::create_dir_all(&self.config_dir).with_whatever_context(|_| {
             format!("create directory {}", self.config_dir.display())
         })?;
-        std::fs::create_dir_all(&self.cache_dir).with_whatever_context(|_| {
-            format!("create directory {}", self.cache_dir.display())
+        std::fs::create_dir_all(&self.asset_cache_dir).with_whatever_context(|_| {
+            format!("create directory {}", self.asset_cache_dir.display())
         })?;
         std::fs::create_dir_all(&self.data_dir).with_whatever_context(|_| {
             format!("create directory {}", self.data_dir.display())
@@ -76,9 +79,9 @@ impl DirectoryPaths {
         self.config_dir.as_path()
     }
 
-    /// Returns the directory that should hold cache & temporary files.
-    pub fn cache_dir(&self) -> &Path {
-        self.cache_dir.as_path()
+    /// Returns the directory that should hold asset cache & temporary files.
+    pub fn asset_cache_dir(&self) -> &Path {
+        self.asset_cache_dir.as_path()
     }
 
     /// Returns the directory that should hold data files.
