6529e8803f09fe88033e8cf038e4ddea5d150e4f
chillfish8 <hburt2003@gmail.com>
2026-05-28 00:46:35 +0100

    Cleanup image warning

--- bluebottle-ui/src/widget/image.rs
+++ bluebottle-ui/src/widget/image.rs
@@ -46,7 +46,6 @@ pub fn thumbnail(handle: Handle) -> Image {
         .width(270)
         .height(152)
         .content_fit(ContentFit::Cover)
-        .into()
 }
 
 /// Creates a loading skeleton for the thumbnail form factor.
@@ -63,7 +62,6 @@ pub fn square(handle: Handle) -> Image {
         .width(152)
         .height(152)
         .content_fit(ContentFit::Cover)
-        .into()
 }
 
 /// Creates a loading skeleton for the square form factor.
